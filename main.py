83167551efabc050bca2d08bac925f2781aaeeda
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2023-04-02 07:54:57 -0700

    WAT HDCOG

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     print(f'Registering cog: {filename[:-3]}')
     bot.load_extension(f'cogs.{filename[:-3]}')
-    
+
+# WATCH_DOGS HIT VDIEO GAME????????
 def watch_cogs(src): 
    filename = os.path.basename(src)
    if filename.endswith('.py'):
@@ -27,4 +28,5 @@ def watch_cogs(src):
       bot.reload_extension(f'cogs.{filename[:-3]}')
 
 cog_watcher = setup_watchdog(watch_cogs, "cogs/")
+
 bot.run(os.getenv("BOT_TOKEN"))
