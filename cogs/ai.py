a9d2d485d596f831c645468fca817f1ae011b106
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2023-04-01 22:27:43 -0700

    formatting

--- cogs/ai.py
+++ cogs/ai.py
@@ -11,21 +11,21 @@ class AI(commands.Cog):
         self.client = client
     
     class PromptFlags(commands.FlagConverter):
-        prompt:          str = commands.flag(description='Main prompt (what you want)')
-        negative_prompt: str = commands.flag(default="nsfw", description='Negative prompt (what you DONT want)')
+        prompt:          str = commands.flag(description="Main prompt (what you want)")
+        negative_prompt: str = commands.flag(default="nsfw", description="Negative prompt (what you DONT want)")
         
-        steps:           int = commands.flag(default=26, description='Amount of steps it will pass through the model')
-        cfg_scale:       int = commands.flag(default=12, description='classifier free guidance, defines how much the image generation process follows the text prompt')
+        steps:           int = commands.flag(default=26, description="Amount of steps it will pass through the model")
+        cfg_scale:       int = commands.flag(default=12, description="classifier free guidance, defines how much the image generation process follows the text prompt")
         
         width:           int = commands.flag(default=512)
         height:          int = commands.flag(default=512)
 
         sampler_index:   str = commands.flag(default="Euler", description="What sampler to use (use /sdsamplers)")
 
-    @commands.hybrid_command(name='sdprompt')
+    @commands.hybrid_command(name="sdprompt")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def sd_prompt(self, ctx, *, flags: PromptFlags):
-        await ctx.reply('Please wait you dipfuck')
+        await ctx.reply("Please wait you dipfuck")
         
         prompt = {
             "prompt": flags.prompt,
@@ -40,16 +40,16 @@ class AI(commands.Cog):
             "sampler_index": flags.sampler_index
         }
         
-        r = sdapi.txt2img(os.getenv('SD_ENDPOINT'), prompt).json()
+        r = sdapi.txt2img(os.getenv("SD_ENDPOINT"), prompt).json()
     
-        for i in r['images']:
+        for i in r["images"]:
             file = discord.File(io.BytesIO(base64.b64decode(i.split(",",1)[0])), filename="output.png")
             await ctx.reply(file=file)
 
-    @commands.hybrid_command(name='sdmodels')
+    @commands.hybrid_command(name="sdmodels")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def sd_models(self, ctx):
-        r = sdapi.get_models(os.getenv('SD_ENDPOINT')).json()
+        r = sdapi.get_models(os.getenv("SD_ENDPOINT")).json()
         reply = "Available models: ```"
         for i in r:
             reply = reply + f'{i["title"]}\n'
@@ -57,10 +57,10 @@ class AI(commands.Cog):
 
         await ctx.reply(reply)
 
-    @commands.hybrid_command(name='sdsamplers')
+    @commands.hybrid_command(name="sdsamplers")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def sd_samplers(self, ctx):
-        r = sdapi.get_samplers(os.getenv('SD_ENDPOINT')).json()
+        r = sdapi.get_samplers(os.getenv("SD_ENDPOINT")).json()
         reply = "Available samplers: ```"
         for i in r:
             reply = reply + f'{i["name"]}\n'
--- utils/predicates.py
+++ utils/predicates.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 def is_owner():
     async def predicate(ctx):
-        return ctx.author.id == os.getenv('BOT_OWNER') 
+        return ctx.author.id == os.getenv("BOT_OWNER") 
     return commands.check(predicate)
 
 def is_in_guild(gid):
