65ae31e0ce48823983456408eca83255395c1142
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2023-04-05 21:17:10 -0700

    i have become functional, destroyer of syntax

--- cogs/ai.py
+++ cogs/ai.py
@@ -7,7 +7,7 @@ import json
 import typing
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import option
 
 SD_ENDPOINT = os.getenv("SD_ENDPOINT")
@@ -15,27 +15,61 @@ SD_ENDPOINT = os.getenv("SD_ENDPOINT")
 class AI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.invalidate_sdcache.start()
 
     @staticmethod
-    def sampler_autocomplete(self):
-        samplers = []
-        for sampler in sdapi.get_samplers(SD_ENDPOINT).json():
-            samplers.append(sampler["name"])
+    def samplers_autocomplete(self, ctx):
+        # maps all samplers' names to a list
+        samplers = list(
+            filter(
+                # filters it for fuzzy serach
+                lambda x: x.lower().startswith(ctx.value.lower()),
+                # maps the json response into a list
+                map(
+                    lambda i: i['name'],
+                    self.sampler_json
+                )
+            )
+        )
+
         return samplers
     
     @staticmethod
-    def model_autocomplete(self):
-        models = []
-        for model in sdapi.get_models(SD_ENDPOINT).json():
-            models.append(model["title"])
+    def models_autocomplete(self, ctx):
+        models = list(
+            filter(
+                # filters it for fuzzy serach
+                lambda x: x.lower().startswith(ctx.value.lower()),
+                # maps the json response into a list
+                map(
+                    lambda i: i['title'],
+                    self.models_json
+                )
+            )
+        )
+
         return models
 
     @staticmethod
-    def style_autocomplete(self):
-        styles = []
-        for style in sdapi.get_styles(SD_ENDPOINT).json():
-            styles.append(style["name"])
+    def styles_autocomplete(self, ctx):
+        styles = list(
+            filter(
+                # filters it for fuzzy serach
+                lambda x: x.lower().startswith(ctx.value.lower()),
+                # maps the json response into a list
+                map(
+                    lambda i: i['name'],
+                    self.styles_json
+                )
+            )
+        )
         return styles
+    
+    @tasks.loop(seconds=30)
+    async def invalidate_sdcache(self):
+        self.sampler_json = sdapi.get_samplers(SD_ENDPOINT).json()
+        self.models_json = sdapi.get_models(SD_ENDPOINT).json()
+        self.styles_json = sdapi.get_styles(SD_ENDPOINT).json()
 
     @commands.slash_command(name="sdprompt")
     @commands.cooldown(1, 10, commands.BucketType.user)
@@ -45,10 +79,10 @@ class AI(commands.Cog):
     @option("cfg_scale",        description="Classifier Free Guidance, defines how much the model should follow the text", default=12, max=36)
     @option("width",            default=512, max=1024)
     @option("height",           default=512, max=1024)
-    @option("sampler",          autocomplete=sampler_autocomplete, default="Euler")
-    @option("styles",           autocomplete=style_autocomplete, default="")
+    @option("sampler",          autocomplete=samplers_autocomplete, default="Euler")
+    @option("styles",           autocomplete=styles_autocomplete, default="")
     @option("seed",             default=-1)
-    async def sd_prompt(self, ctx: discord.ApplicationContext, prompt: str, negative_prompt: str, steps: int, cfg_scale: int, width: int, height: int, sampler: str, styles: str, seed: int):
+    async def sd_prompt(self, ctx: discord.ApplicationContext, prompt: str, negative_prompt: str, steps: int, cfg_scale: float, width: int, height: int, sampler: str, styles: str, seed: int):
         # omg so unprofeshunul 
         await ctx.respond("Please wait while we generate your ~~porn~~ image")
         
@@ -89,7 +123,7 @@ class AI(commands.Cog):
 
     @commands.slash_command(name="sdmodel")
     @commands.max_concurrency(1, commands.BucketType.guild)
-    @option("model", autocomplete=model_autocomplete, description="Model you want")
+    @option("model", autocomplete=models_autocomplete, description="Model you want")
     async def sd_model(self, ctx: discord.ApplicationCommand, model: str):
         payload = {
             "sd_model_checkpoint": model
