214e076c4ab830ef79b49367b9fd876c1cfe92b0
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2023-06-11 06:23:56 -0700

    woops weird bug

--- utils/lmapi.py
+++ utils/lmapi.py
@@ -1,5 +1,5 @@
 import requests
-from common_async import async_post
+from utils.common_async import async_post
 
 def generate(endpoint, payload):
     return requests.post(url=f"{endpoint}/api/v1/generate", json=payload)
