2e9fa5645f40645b10ecb40bed5852951a9fe8e8
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2023-06-18 02:03:43 -0700

    overkill error casync handling

--- utils/casync.py
+++ utils/casync.py
@@ -1,21 +1,34 @@
 import aiohttp
 
+# this is defo overkill
+class ResponseError(Exception):
+    def __init__(self, status: int):
+        self.status = status
+        super().__init__(self.status)
+
+def error_handler(exception):
+    if isinstance(exception, ResponseError):
+        return None, exception.status
+
+    if isinstance(exception, aiohttp.ClientConnectorError):
+        return None, exception.errno
+
 async def async_post(endpoint: str, secondary: str, payload: dict):
     async with aiohttp.ClientSession() as session:
         try: # it throws an error if it flat out cannot connect, or otherwise.
             async with session.post(url=f"{endpoint}{secondary}", json=payload) as resp:
-                if resp.status == 200:
-                    return await resp.json(), resp.status
-            return None, resp.status
-        except aiohttp.ClientConnectorError as e:
-            return None, e.errno
+                if resp.status != 200:
+                    raise ResponseError(resp.status)
+            return await resp.json(), resp.status
+        except Exception as e:
+            return error_handler(e)
 
 async def async_get(endpoint: str, secondary: str):
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url=f"{endpoint}{secondary}") as resp:
-                if resp.status == 200:
-                    return await resp.json(), resp.status
-                return None, resp.status
-        except aiohttp.ClientConnectorError as e:
-            return None, e.errno
+                if resp.status != 200:
+                    raise ResponseError(resp.status)
+            return await resp.json(), resp.status
+        except Exception as e:
+            return error_handler(e)
